e5b872a535a4ba7d0db22cd1c98d9b183d1132ea
Patrick Bowes <patrick@patricks-mbp.myfiosgateway.com>
2022-05-04 18:29:07 -0400

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,7 +15,6 @@ breads.get('/', (req, res) => {
 })
 
 // CREATE
-// CREATE
 breads.post('/', (req, res) => {
     if (!req.body.image) {
         req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
@@ -29,7 +28,6 @@ breads.post('/', (req, res) => {
     res.redirect('/breads')
 })
 
-
 // NEW
 breads.get('/new', (req, res) => {
     res.render('new')
@@ -39,13 +37,19 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
         res.render('Show', {
-            bread: Bread[req.params.arrayIndex]
+            bread: Bread[req.params.arrayIndex],
+            index: req.params.arrayIndex,
         })
     } else {
         res.render('404')
     }
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+    Bread.splice(req.params.indexArray, 1)
+    res.status(303).redirect('/breads')
+})
 
 module.exports = breads
 
